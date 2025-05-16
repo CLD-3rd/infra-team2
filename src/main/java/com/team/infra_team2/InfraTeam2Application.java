6dced5ffd2b588c4047addf7709126aee8375e41
alex052525 <alex62626@naver.com>
2025-05-16 22:52:31 +0900

    chore: Spring Boot project 생성

---
+++ src/main/java/com/team/infra_team2/InfraTeam2Application.java
@@ -0,0 +1,13 @@
+package com.team.infra_team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraTeam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/infra_team2/InfraTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.infra_team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraTeam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
