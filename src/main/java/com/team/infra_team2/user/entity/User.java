7359a41662be77c30019415610259a884f2f4255
alex052525 <alex62626@naver.com>
2025-05-18 10:43:55 +0900

    User 엔티티 생성

---
+++ src/main/java/com/team/infra_team2/user/entity/User.java
@@ -0,0 +1,22 @@
+package com.team.infra_team2.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	@Column(name = "user_id")
+	private Integer id;
+	
+	private String password;
+	
+	private UserRole role;
+	
+}
