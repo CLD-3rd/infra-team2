8bc9a4fa8651fa750392c62ef82ad16b57c827f5
ghd5100 <ghd5100@gmail.com>
2025-05-19 15:56:08 +0900

    로그인/회원가입 초기세팅

---
+++ src/main/java/com/team/infra_team2/user/controller/SecurityController.java
@@ -0,0 +1,86 @@
+package com.team.infra_team2.user.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.team.infra_team2.user.constant.UserRoleType;
+import com.team.infra_team2.user.entity.User;
+import com.team.infra_team2.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Controller
+public class SecurityController {
+
+private final UserRepository userRepository;
+// 암호화 모듈 주입
+private final PasswordEncoder passwordEncoder;
+
+@GetMapping({ "", "/" })
+public String indexPage() {
+return "index";
+}
+
+@GetMapping("/user")
+public String userPage() {
+return "user";
+}
+
+@GetMapping("/admin")
+public String adminPage() {
+return "admin";
+}
+
+@GetMapping("/manager")
+public String manager() {
+return "manager";
+}
+
+@GetMapping("/login")
+public String loginPage() {
+return "login";
+}
+
+
+@GetMapping("/signup")
+public String signUpPage() {
+
+return "signUp";
+}
+
+@PostMapping("/signup")
+public String signUp(User user) {
+
+ 
+
+//비밀번호 암호화
+String rawPassword=user.getPassword(); 
+System.out.println(rawPassword);
+String encrptedOassword = passwordEncoder.encode(rawPassword);
+System.out.println(encrptedOassword);
+
+
+//객체 저장 중
+user.setPassword(encrptedOassword);
+user.setUser_role_type(UserRoleType.ADMIN); 
+userRepository.save(user);
+
+return "redirect:/";
+}
+
+
+//@PreAuthorize("hasRole('ROLE_USER)') or hasRole('ROLE_MANAGER')")
+@Secured(value= {"ROLE_USER", "ROLE_MANAGER"}) //실행되기전에 권한점검
+@GetMapping("/about")
+public String aboutPage() { //매소드에 권한부여
+
+return "about";
+}
+}
--- src/main/java/com/team/infra_team2/user/entity/User.java
+++ src/main/java/com/team/infra_team2/user/entity/User.java
@@ -1,28 +1,45 @@
 package com.team.infra_team2.user.entity;
 
 import com.team.infra_team2.common.entity.BaseEntity;
-import com.team.infra_team2.user.constant.UserRole;
+import com.team.infra_team2.user.constant.UserRoleType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+@Data
 @Entity
 public class User extends BaseEntity {
 	
 	@Id
-	@Column(name = "user_id")
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long user_id;
+	
+	private String username;
 	
 	private String password;
-	
+
 	@Enumerated(EnumType.STRING)
-	private UserRole role;
+	private UserRoleType user_role_type;
+	
+	protected User() {} 
+
+    private User(String username, String password, UserRoleType userRoleType) {
+        this.username = username;
+        this.password = password;
+        this.user_role_type = userRoleType;
+    }
+
+    public static User of(String username, String password, UserRoleType userRoleType) {
+        return new User(username, password, userRoleType);
+    }
 	
 }
