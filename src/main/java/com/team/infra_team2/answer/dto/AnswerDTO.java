cabd61af92dbe53dcc30bde323675e2420dffed5
alex052525 <alex62626@naver.com>
2025-05-19 15:47:05 +0900

    AnswerDTO 생성

---
+++ src/main/java/com/team/infra_team2/answer/dto/AnswerDTO.java
@@ -0,0 +1,14 @@
+package com.team.infra_team2.answer.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AnswerDTO {
+	private Long answer_id;
+	private Integer selected_answer;
+	private Boolean is_correct;
+	private Long question_id;
+	private Long solve_id;
+	private Long user_id;
+	
+}
