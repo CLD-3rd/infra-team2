a43c8cc5439137995106af26bbcce4f75a253cb8
alex052525 <alex62626@naver.com>
2025-05-19 15:16:56 +0900

    feat: 문제 단건 제출 요청, 응답 DTO 생성

---
+++ src/main/java/com/team/infra_team2/answer/dto/request/AnswerSubmitDetailRequestDTO.java
@@ -0,0 +1,11 @@
+package com.team.infra_team2.answer.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class AnswerSubmitDetailRequestDTO {
+	
+	private Long question_id;
+	private Long selected_answer;
+	
+}
---
+++ src/main/java/com/team/infra_team2/answer/dto/response/AnswerSubmitDetailResponseDTO.java
@@ -0,0 +1,11 @@
+package com.team.infra_team2.answer.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class AnswerSubmitDetailResponseDTO {
+	
+	private Long answer_id;
+	private String message;
+
+}
