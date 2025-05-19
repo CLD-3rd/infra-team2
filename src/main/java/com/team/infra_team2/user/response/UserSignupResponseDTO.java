57050330636789272d20751e37bfc8ca8be946ae
ghd5100 <ghd5100@gmail.com>
2025-05-19 15:58:26 +0900

    로그인/회원가입 초기세팅

---
+++ src/main/java/com/team/infra_team2/user/response/UserSignupResponseDTO.java
@@ -0,0 +1,22 @@
+package com.team.infra_team2.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+
+public class UserSignupResponseDTO {
+
+    private String username;
+    private String message;
+
+    public UserSignupResponseDTO(String username, String message) {
+        this.username = username;
+        this.message = message;
+    }
+
+    public static UserSignupResponseDTO of(String username) {
+        return new UserSignupResponseDTO(username, "회원가입이 완료되었습니다.");
+    }
+}
