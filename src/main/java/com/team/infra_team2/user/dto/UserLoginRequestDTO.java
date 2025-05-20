75405679c6e207a9c7f43eb507fa9bddaba32982
kaiju782 <didwjdah7@sunmoon.ac.kr>
2025-05-20 17:06:51 +0900

    Revert "Merge pull request #20 from CLD-3rd/feature/solve-api"
    This reverts commit c08b8b107b9d6c07da62a4d76fa8e52096008d52, reversing
    changes made to 67649358183363b6634e426674ef660f03a26056.

--- src/main/java/com/team/infra_team2/user/dto/UserLoginRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.team.infra_team2.user.dto;
-
-public class UserLoginRequestDTO {
-	private String username;
-    private String password;
-}
--- src/main/java/com/team/infra_team2/user/request/UserLoginRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.team.infra_team2.user.request;
-
-public class UserLoginRequestDTO {
-
-    private String username;
-    private String password;
-}
