527b0e23e852f2d6462b1205bd269cd4fc4be097
ghd5100 <ghd5100@gmail.com>
2025-05-20 21:30:25 +0900

    admin데이터 포함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,8 @@
 -- user 테이블
 INSERT INTO user (created_at, updated_at, password, username, user_role_type)
 VALUES ('2025-05-01 10:00:00', '2025-05-01 10:00:00', 'pass123', 'user1', 'USER'),
-       ('2025-05-02 12:00:00', '2025-05-02 12:00:00', 'pass456', 'user2', 'ADMIN');
+       ('2025-05-02 12:00:00', '2025-05-02 12:00:00', 'pass456', 'user2', 'USER'),
+	   ('2025-05-20 21:00:00', '2025-05-20 21:00:00', '$2a$10$QYawTNiT9AVUfcydUUylB.pnO1estzITEMlYmx.huA7Rcu63auEWW', 'admin', 'ADMIN');
 
 -- question 테이블
 INSERT INTO question (correct_answer, created_at, updated_at, user_id, question_text)
@@ -30,8 +31,8 @@ VALUES (1, 2, '2025-05-05 14:00:00', '2025-05-05 14:00:00', 1, 1, 1),
        (0, 3, '2025-05-05 15:00:00', '2025-05-05 15:00:00', 2, 1, 2);
 
 
-ALTER TABLE user AUTO_INCREMENT = 1;
-ALTER TABLE question AUTO_INCREMENT = 1;
-ALTER TABLE choice AUTO_INCREMENT = 1;
-ALTER TABLE solve AUTO_INCREMENT = 1;
-ALTER TABLE answer AUTO_INCREMENT = 1;
+ALTER TABLE user AUTO_INCREMENT = 3;
+ALTER TABLE question AUTO_INCREMENT = 3;
+ALTER TABLE choice AUTO_INCREMENT = 3;
+ALTER TABLE solve AUTO_INCREMENT = 3;
+ALTER TABLE answer AUTO_INCREMENT = 3;
